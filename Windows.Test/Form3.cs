eae99b0341329ce6a46f02f14a59940258334567
rinack <rinack@msn.com>
2015-03-19 17:31:22 +0800

    modified

--- Windows.Test/Form3.cs
+++ Windows.Test/Form3.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Windows.Forms.Controls.Forms.Metro;
+using Windows.Forms.Controls.MetroForm;
 
 namespace Windows.Test
 {
