305e0657eb159c084a41e1780bf50886170a14c6
rinack <rinack@msn.com>
2015-03-20 11:47:21 +0800

    Modified

--- Windows.Forms/Controls/StyleForm/MetroForm.cs
+++ Windows.Forms/Controls/StyleForm/MetroForm.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Windows.Forms.Controls.Enums;
+using Windows.Forms.Controls.Methods;
 using Windows.Forms.Controls.WinAPI;
 using Windows.Forms.Controls.WinForm;
 
@@ -14,6 +16,8 @@ namespace Windows.Forms.Controls.StyleForm
 {
     public partial class MetroForm : BaseForm
     {
+        private Windows.Forms.Controls.MetroForm.MainForm skin;
+
         public MetroForm()
             : base()
         {
@@ -25,8 +29,18 @@ namespace Windows.Forms.Controls.StyleForm
         /// <summary>
         /// 边框图片
         /// </summary>
-        private Image _borderImage = AssemblyHelper.GetImage("StanForm.FormFrame.fringe_bkg.png");
+        private Image _borderImage = AssemblyHelper.GetImage("StanForm.FormFrame.Metro_fringe_bkg.png");
         #endregion
+
+        private bool isRadius = false;
+        /// <summary>
+        /// 是否设置圆角
+        /// </summary>
+        public bool IsRadius
+        {
+            get { return isRadius; }
+            set { isRadius = value; }
+        }
 
         #region 属性
         /// <summary>
@@ -150,9 +164,89 @@ namespace Windows.Forms.Controls.StyleForm
         protected override void OnResize(EventArgs e)
         {
             base.OnResize(e);
-            //调用API，将窗体剪成圆角
-            int rgn = NativeMethods.CreateRoundRectRgn(0, 0, this.Width + 1, this.Height + 1, 4, 4);
-            NativeMethods.SetWindowRgn(this.Handle, rgn, true);
+
+            if (isRadius)
+            {
+                //调用API，将窗体剪成圆角
+                int rgn = NativeMethods.CreateRoundRectRgn(0, 0, this.Width + 1, this.Height + 1, 4, 4);
+                NativeMethods.SetWindowRgn(this.Handle, rgn, true);
+            }
+        }
+
+        #endregion
+
+        #region 重载事件
+        //Show或Hide被调用时
+        protected override void OnVisibleChanged(EventArgs e)
+        {
+            if (Visible)
+            {
+                //启用窗口淡入淡出
+                if (!DesignMode)
+                {
+                    //淡入特效
+                    Win32.AnimateWindow(this.Handle, 150, Win32.AW_BLEND | Win32.AW_ACTIVATE);
+                }
+                //判断不是在设计器中
+                if (!DesignMode && skin == null)
+                {
+                    skin = new Windows.Forms.Controls.MetroForm.MainForm(this);
+                    skin.Show(this);
+                }
+                base.OnVisibleChanged(e);
+            }
+            else
+            {
+                base.OnVisibleChanged(e);
+                Win32.AnimateWindow(this.Handle, 150, Win32.AW_BLEND | Win32.AW_HIDE);
+            }
+        }
+
+        //窗体关闭时
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            //先关闭阴影窗体
+            if (skin != null)
+            {
+                skin.Close();
+            }
+            //在Form_FormClosing中添加代码实现窗体的淡出
+            Win32.AnimateWindow(this.Handle, 150, Win32.AW_BLEND | Win32.AW_HIDE);
+        }
+
+        //控件首次创建时被调用
+        protected override void OnCreateControl()
+        {
+            base.OnCreateControl();
+            if (isRadius)
+            {
+                SetReion();
+            }
+        }
+
+        //圆角
+        private void SetReion()
+        {
+            using (GraphicsPath path =
+                    GraphicsPathHelper.CreatePath(
+                    new Rectangle(Point.Empty, base.Size), 6, RoundStyle.All, true))
+            {
+                Region region = new Region(path);
+                path.Widen(Pens.White);
+                region.Union(path);
+                this.Region = region;
+            }
+        }
+
+        //改变窗体大小时
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            if (isRadius)
+            {
+                SetReion();
+            }
         }
 
         #endregion
