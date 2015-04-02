2ef84b2cc52d9c9a337a337370ad3f4387e9119e
rinack <rinack@msn.com>
2015-04-02 17:40:28 +0800

    update

--- Windows.Test/Form5.cs
+++ Windows.Test/Form5.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Windows.Forms;
 using Windows.Forms.Controls.StyleForm;
 
 namespace Windows.Test
@@ -16,6 +17,103 @@ namespace Windows.Test
         public Form5()
         {
             InitializeComponent();
+
+            image = AssemblyHelper.GetImage("Icons.start.png");
+            Init();
+        }
+
+        private string _toolTipTitle = "ToolTipEx示例";
+        private string _toolTip =
+            "CS 程序员之窗-ToolTipEx示例-{0}。\r\n" +
+            "                    ——Statts_2000\r\n" +
+            "                         2010.01.09";
+        private Image image;
+
+        private void SetToolTip(Control control, string tip)
+        {
+            toolTipEx.SetToolTip(
+                control,
+                string.Format(_toolTip, tip));
+        }
+
+        private void ResetToolTip()
+        {
+            toolTipEx.Active = false;
+            toolTipEx.Opacity = 1D;
+            toolTipEx.ImageSize = new Size(16, 16);
+            toolTipEx.Image = null;
+            toolTipEx.ToolTipTitle = "";
+        }
+
+        private void Init()
+        {
+            toolTipEx.Active = false;
+            SetToolTip(label1, "无标题");
+            SetToolTip(label2, "系统图标");
+            SetToolTip(label3, "自定义图标");
+            SetToolTip(label4, "大图标");
+            SetToolTip(label5, "透明");
+
+            label1.MouseEnter += delegate(object sender, EventArgs e)
+            {
+                toolTipEx.Active = true;
+            };
+
+            label2.MouseEnter += delegate(object sender, EventArgs e)
+            {
+                toolTipEx.ToolTipIcon = ToolTipIcon.Info;
+                toolTipEx.ToolTipTitle = _toolTipTitle;
+                toolTipEx.Active = true;
+            };
+
+            label3.MouseEnter += delegate(object sender, EventArgs e)
+            {
+                toolTipEx.Image = image;
+                toolTipEx.ToolTipTitle = _toolTipTitle;
+                toolTipEx.Active = true;
+            };
+
+            label4.MouseEnter += delegate(object sender, EventArgs e)
+            {
+                toolTipEx.ImageSize = new Size(32, 32);
+                toolTipEx.Image = image;
+                toolTipEx.ToolTipTitle = _toolTipTitle;
+                toolTipEx.Active = true;
+            };
+
+            label5.MouseEnter += delegate(object sender, EventArgs e)
+            {
+                toolTipEx.Opacity = 0.7D;
+                toolTipEx.ImageSize = new Size(24, 24);
+                toolTipEx.Image = image;
+                toolTipEx.ToolTipTitle = _toolTipTitle;
+                toolTipEx.Active = true;
+            };
+
+            label1.MouseLeave += delegate(object sender, EventArgs e)
+            {
+                ResetToolTip();
+            };
+
+            label2.MouseLeave += delegate(object sender, EventArgs e)
+            {
+                ResetToolTip();
+            };
+
+            label3.MouseLeave += delegate(object sender, EventArgs e)
+            {
+                ResetToolTip();
+            };
+
+            label4.MouseLeave += delegate(object sender, EventArgs e)
+            {
+                ResetToolTip();
+            };
+
+            label5.MouseLeave += delegate(object sender, EventArgs e)
+            {
+                ResetToolTip();
+            };
         }
     }
 }
--- Windows.Test/Form5.designer.cs
+++ Windows.Test/Form5.designer.cs
@@ -29,10 +29,90 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTipEx = new Windows.Forms.Controls.ToolTips.ToolTipEx(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // toolTipEx
+            // 
+            this.toolTipEx.AutoPopDelay = 5000;
+            this.toolTipEx.InitialDelay = 500;
+            this.toolTipEx.OwnerDraw = true;
+            this.toolTipEx.ReshowDelay = 800;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form5";
             this.Text = "Form5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Forms.Controls.ToolTips.ToolTipEx toolTipEx;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
