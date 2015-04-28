223bd1fb1c666ee94e6119877e86a47319110f45
rinack <rinack@msn.com>
2015-04-28 17:51:14 +0800

    update

--- Windows.Test/Program.cs
+++ Windows.Test/Program.cs
@@ -16,7 +16,7 @@ namespace Windows.Test
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form2());
+            Application.Run(new DemoForm());
         }
     }
 }
